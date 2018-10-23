0cf3d78000136fce767d483af27ef26604732524
suryaadtmaja <suryaadtmaja13@gmail.com>
2018-10-23 18:16:26 +0700

    update readme

--- resources/js/count.js
+++ resources/js/count.js
@@ -1,6 +1,6 @@
 // Set the date we're counting down to
+//  Change here
 var countDownDate = new Date("Oct 29, 2018 15:37:25").getTime();
-
 // Update the count down every 1 second
 var x = setInterval(function() {
 
